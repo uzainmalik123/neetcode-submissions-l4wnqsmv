2d1082c9270a2366ccab8714ac10e1c0426fd5fa
Uzain Ahmed <2k24-it-218@student.sau.edu.pk>
2026-09-02 15:05:49 +0500

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        mp = {']':'[', '}':'{', ')':'('}
+        stack = []
+
+        for c in s:        
+            if c in mp:
+                if stack and stack[-1] == mp[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+
+        return False if stack else True
